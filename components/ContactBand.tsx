d2d18c672be77c2cd4b9318453a70e4aa9da0f4e
liakouran <elias.sarigiannidis@gmail.com>
2026-06-04 23:52:29 +0300

    Update ContactBand.tsx

--- components/ContactBand.tsx
+++ components/ContactBand.tsx
@@ -24,10 +24,13 @@ export default async function ContactBand() {
           </a>
         </div>
         <iframe
+          className="h-[450px] w-full rounded-lg"
           title="Thessaloniki map"
-          src="https://maps.app.goo.gl/dMas23Zo6aYvVo948"
-          className="min-h-96 w-full border-0 grayscale"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3027.6619987869335!2d22.94661507750159!3d40.63733547140493!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14a839397ce3b2d1%3A0x611c61348de8b85b!2snuma%20hair%20salon!5e0!3m2!1sel!2sgr!4v1780606160488!5m2!1sel!2sgr"
+          style={{ border: 0 }}
+          allowFullScreen
           loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
         />
       </div>
     </Section>
